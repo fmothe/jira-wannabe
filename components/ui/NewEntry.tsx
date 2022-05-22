5c7bfef295a3dbe0c3bcb234c2aab6419693f4b6
fmothe <federicom@epidataconsulting.com>
2022-05-22 01:56:27 -0300

    feat(Root): wrapped icon with button

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -2,28 +2,30 @@ import { Box, Button, TextField } from "@mui/material";
 import React, { ChangeEvent, useState, useContext } from "react";
 import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
-import { EntriesContext } from '../../context/entries';
-import { UIContext } from '../../context/ui/UIContext';
+import { EntriesContext } from "../../context/entries";
+import { UIContext } from "../../context/ui/UIContext";
 export const NewEntry = () => {
-    const {addNewEntry} = useContext(EntriesContext);
-    const {setIsAddingEntry, isAddingEntry} = useContext(UIContext)
-    const [inputValue, setInputValue] = useState('');
+    const { addNewEntry } = useContext(EntriesContext);
+    const { setIsAddingEntry, isAddingEntry } = useContext(UIContext);
+    const [inputValue, setInputValue] = useState("");
+    const [titleValue, setTitleValue] = useState("");
     const [touched, setTouched] = useState(false);
 
     const onTextChange = (event: ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
-    }
+    };
+    const onTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitleValue(event.target.value);
+    };
 
     const onSave = () => {
-        if(inputValue.length === 0) return
+        if (titleValue.length === 0) return;
 
-        // addNewEntry(inputValue)
+        addNewEntry(inputValue, titleValue);
         setIsAddingEntry(false);
         setTouched(false);
-        setInputValue(''); 
-
-
-    }
+        setInputValue("");
+    };
 
     return (
         <Box sx={{ marginBottom: 2, paddingX: 2 }}>
@@ -32,11 +34,32 @@ export const NewEntry = () => {
                     <TextField
                         fullWidth
                         sx={{ marginTop: 2, marginBottom: 1 }}
+                        placeholder="Title"
+                        autoFocus
+                        multiline
+                        label="New Entry"
+                        helperText={
+                            titleValue.length <= 0 &&
+                            touched &&
+                            "Please enter a task"
+                        }
+                        error={titleValue.length <= 0 && touched}
+                        value={titleValue}
+                        onChange={onTitleChange}
+                        onBlur={() => setTouched(true)}
+                    />
+                    <TextField
+                        fullWidth
+                        sx={{ marginTop: 2, marginBottom: 1 }}
                         placeholder="New Entry"
                         autoFocus
                         multiline
                         label="New Entry"
-                        helperText={inputValue.length <= 0 && touched && 'Please enter a task'}
+                        helperText={
+                            inputValue.length <= 0 &&
+                            touched &&
+                            "Please enter a task"
+                        }
                         error={inputValue.length <= 0 && touched}
                         value={inputValue}
                         onChange={onTextChange}
